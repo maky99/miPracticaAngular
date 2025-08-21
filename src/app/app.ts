1ffef2110c1f20dfd910448620df9d09d59684a1
maky99 <makyade99@gmail.com>
2025-08-21 08:12:21 -0300

    vista principal, tabla y navbar colocados

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponet } from './component/navbar/navbar';
+import { ListComponent } from './component/list/list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponet, ListComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
   protected readonly title = signal('miPracticaAngular');
---
+++ src/app/component/list/list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list',
+  imports: [],
+  templateUrl: './list.html',
+  styleUrls: ['./list.css']
+})
+export class ListComponent {
+
+}
